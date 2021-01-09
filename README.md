0380043988cea4e7c9dca235ec31b74f4151036e
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2021-01-09 20:24:58 +1030

    change column widths and update readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# employee_directory
- 
+# Employee Directory
+
+## Description
+ This is a React.js application that gets data about random employees and displays them allowing for filtering and searching of employees.
+![demo](/screenshots/demo.gif)   
+
+
+## Table of Contents
+* [Description](#Description)
+* [Installation](#Installation)
+* [Features](#Features)
+* [Questions](#Questions)
+## Installation
+```
+npm install
+
+```
+
+run locally in the command line:
+``` 
+npm start
+```
+## Features 
+Employee data is stored in state and passed down into child elements.   
+Allows for filtering and sorting of all columns using material-ui.   
+All employee fields and values are searchable using the search input and updated after each key stroke.  
+
+## Questions
+[github](https://github.com/nick75mowbray)
--- employees/src/components/Grid.js
+++ employees/src/components/Grid.js
@@ -30,15 +30,22 @@ function pullData(item, index) {
 const rows: RowsProp = userRows;
 
 const columns: ColDef[] = [
-  { field: 'col1', headerName: 'First Name', width: '15vw' },
-  { field: 'col2', headerName: 'Last Name', width: '20vw' },
-  { field: 'col3', headerName: 'Phone', width: '20vw' },
-  { field: 'col4', headerName: 'Email', width: '20vw' },
+  { field: 'col1', headerName: 'First Name', width: 100 },
+  { field: 'col2', headerName: 'Last Name', width: 130 },
+  { field: 'col3', headerName: 'Phone', width: 140 },
+  { field: 'col4', headerName: 'Email', width: 250 },
 ];
 
   return (
-    <div style={{ height: '90vh', width: '95vw' }}>
-      <DataGrid rows={rows} columns={columns} />
+    <div>
+
+    {userData ? 
+      <div style={{ height: '90vh', width: '95vw' }}>
+        <DataGrid rows={rows} columns={columns} />
+      </div> 
+      : <p>Oops...Something went wrong, try reloading</p>
+    }
+    
     </div>
   );
 }
